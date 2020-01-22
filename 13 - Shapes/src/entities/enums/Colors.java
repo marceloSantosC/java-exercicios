9ed7561e2bd9d02ca1008ea2ee09d585cc5284c7
Marcelo dos Santos <marcelo17082000@gmail.com>
2020-01-22 18:33:06 -0300

    Renomeadas pastas

--- 13 - Shapes/src/entities/enums/Colors.java
+++
@@ -1,7 +0,0 @@
-package entities.enums;
-
-public enum Colors {
-	BLACK,
-	BLUE,
-	RED;
-}
--- 17 - Processamento de contratos/src/services/paymentService.java
+++
@@ -1,6 +0,0 @@
-package services;
-
-public interface paymentService {
-	double paymentFee(double amount);
-	double interest(double amount, int intallments);
-}
